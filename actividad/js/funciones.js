fef2844876d610f77881801f2c7bb402b6f1d928
shanshurbi <rod.urbina@duocuc.cl>
2024-04-25 01:53:45 -0400

    no me funciona la alerta del mail

--- actividad/js/funciones.js
+++ actividad/js/funciones.js
@@ -6,9 +6,22 @@ function validar() {
         if (rut.length >= 10) {
 
         } else {
-            document.getElementById("resultado").innerHTML = "<div class='alert alert-danger' role='alert'>¡La contraseña debe tener al menos 10 caracteres!</div>";
+            document.getElementById("resultado").innerHTML = "<div class='alert alert-danger' role='alert'>¡EL rut debe tener al menos 10 caracteres!</div>";
         }
     } else {
-        document.getElementById("resultado").innerHTML = "<div class='alert alert-danger' role='alert'>¡El nombre de usuario debe tener al menos 15 caracteres!</div>";
+        document.getElementById("resultado").innerHTML = "<div class='alert alert-danger' role='alert'>¡El nombre debe tener al menos 15 caracteres!</div>";
     }
 }
+
+function validarmail(mail) {
+    const pattern = /^[a-zA-Z0-9._-]+@(gmail|outlook)\.(com|org|net)$/i;
+    let mail = pattern.test(mail);
+
+    if (mail){
+        
+    }
+    else{
+        document.getElementById("resultado").innerHTML = "<div class='alert alert-danger' role='alert'>¡El correo es exclusivamente gmail u outlook!</div>";
+    }
+    return mail;
+  }
